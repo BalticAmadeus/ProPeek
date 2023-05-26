00833f804a3780cd02101e9d9bab2a2447b46484
PauliusKu <p.kuprevicius@ba.lt>
2023-05-26 09:51:46 +0300

    Initial project structure

---
+++ src/web/services/profilerService.ts
@@ -0,0 +1,7 @@
+export class ProfilerService {
+    public async parse(fileName: string) : Promise<void> {
+        // readfile 
+        // parse file <--Haroldas
+        // start webview
+    }
+}
