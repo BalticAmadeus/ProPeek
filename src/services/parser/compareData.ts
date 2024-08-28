845958c4e2eb9b1645b77e10181b18d2d3870057
pjaskutelis <p.jaskutelis@ba.lt>
2024-08-28 15:27:56 +0300

    Changed compare to only use moduleNames when comparing modules

--- src/services/parser/compareData.ts
+++ src/services/parser/compareData.ts
@@ -10,27 +10,20 @@ export async function compareData(
 ): Promise<ComparedData[]> {
   const comparedData: ComparedData[] = [];
   console.log(compareData);
-  const compareModuleMap = new Map<number, ModuleDetails>();
+  const compareModuleMap = new Map<string, ModuleDetails>();
   compareData.moduleDetails.forEach((module) => {
-    compareModuleMap.set(module.moduleID, module);
+    compareModuleMap.set(module.moduleName, module);
   });
 
   presentationData.moduleDetails.forEach((module) => {
-    const comparedModule = compareModuleMap.get(module.moduleID);
+    const comparedModule = compareModuleMap.get(module.moduleName);
 
-    if (comparedModule?.moduleName === module.moduleName) {
-      const timesCalledChange = Number(
-        (module.timesCalled - comparedModule.timesCalled).toFixed(6)
-      );
-      const avgTimePerCallChange = Number(
-        (
-          (module.avgTimePerCall || 0) - (comparedModule.avgTimePerCall || 0)
-        ).toFixed(6)
-      );
-      const totalTimeChange = Number(
-        (module.totalTime - comparedModule.totalTime).toFixed(6)
-      );
-
+    if (comparedModule) {
+      const timesCalledChange = Number((module.timesCalled - comparedModule.timesCalled).toFixed(6));
+      const avgTimePerCallChange = Number(((module.avgTimePerCall || 0) - (comparedModule.avgTimePerCall || 0)).toFixed(6));
+      const totalTimeChange = Number((module.totalTime - comparedModule.totalTime).toFixed(6));
+      console.log("comp", comparedModule);
+      console.log("mod",module);
       if (
         timesCalledChange !== 0 ||
         avgTimePerCallChange !== 0 ||
