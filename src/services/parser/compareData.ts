6311bed495679fd8fc3befcc81ded70f896de6b6
pjaskutelis <p.jaskutelis@ba.lt>
2024-09-24 21:25:04 +0300

    Refactoring code

--- src/services/parser/compareData.ts
+++ src/services/parser/compareData.ts
@@ -18,81 +18,33 @@ export async function compareData(
     const newModule = newModuleMap.get(oldModule[0].moduleName);
 
     if (newModule) {
-      if (newModule.length === oldModule.length) {
-        comparedModules.push(...compareModules(oldModule, newModule));
-        console.log("Old1", oldModule);
-        console.log("new1", newModule);
-      } else {
-        let moduleLength = 0;
-        if (oldModule.length > newModule.length) {
-          moduleLength = newModule.length;
-        } else {
-          moduleLength = oldModule.length;
-        }
+      const moduleLength = Math.min(oldModule.length, newModule.length);
 
-        for (let i = 0; i < moduleLength; i++) {
-          const mergedModuleID =
-            oldModule[i].moduleID * Constants.moduleIdMult +
-            newModule[i].moduleID;
+      comparedModules.push(
+        ...compareModules(oldModule, newModule, moduleLength)
+      );
 
-          const timesCalledChange = calculateChange(
-            newModule[i].timesCalled,
-            oldModule[i].timesCalled
-          );
-          const avgTimePerCallChange = calculateChange(
-            newModule[i].avgTimePerCall || 0,
-            oldModule[i].avgTimePerCall || 0
-          );
-          const totalTimeChange = calculateChange(
-            newModule[i].totalTime,
-            oldModule[i].totalTime
-          );
-          const comparedModule: ComparedModule = {
-            ...oldModule[i],
-            moduleID: mergedModuleID,
-            timesCalledChange,
-            avgTimePerCallChange,
-            totalTimeChange,
-          };
-          comparedModules.push(comparedModule);
-          console.log("Old2", oldModule);
-          console.log("new2", newModule);
-        }
-        if (moduleLength === oldModule.length) {
-          for (let i = moduleLength; i < newModule.length; i++) {
-            comparedModules.push(createAddedModule(newModule[i]));
-            console.log("new3", newModule);
-          }
-        } else {
-          for (let i = moduleLength; i < oldModule.length; i++) {
-            comparedModules.push(createRemovedModule(oldModule[i]));
-            console.log("Old3", oldModule);
-          }
-        }
+      if (moduleLength < newModule.length) {
+        comparedModules.push(
+          ...createAddedModule(newModule.slice(moduleLength))
+        );
       }
+      if (moduleLength < oldModule.length) {
+        comparedModules.push(
+          ...createRemovedModule(oldModule.slice(moduleLength))
+        );
+      }
+
       oldModuleMap.delete(oldModule[0]?.moduleName);
       newModuleMap.delete(newModule[0]?.moduleName);
-      console.log("OldDel", oldModule);
-      console.log("NewDel", newModule);
     } else {
-      oldModule.forEach((module) => {
-        comparedModules.push(createRemovedModule(module));
-        console.log("Old4", oldModule);
-      });
+      comparedModules.push(...createRemovedModule(oldModule));
       oldModuleMap.delete(oldModule[0].moduleName);
     }
   });
   newModuleMap.forEach((modules) => {
-    modules.forEach((module) => {
-      comparedModules.push(createAddedModule(module));
-    });
+    comparedModules.push(...createAddedModule(modules));
     newModuleMap.delete(modules[0].moduleName);
-  });
-  oldModuleMap.forEach((modules) => {
-    console.log("leftOld", modules);
-  });
-  newModuleMap.forEach((modules) => {
-    console.log("leftNew", modules);
   });
   return {
     comparedModules,
@@ -119,11 +71,12 @@ const mapModules = (
 
 const compareModules = (
   oldModule: ModuleDetails[],
-  newModule: ModuleDetails[]
+  newModule: ModuleDetails[],
+  moduleLength: number
 ): ComparedModule[] => {
   const moduleArray: ComparedModule[] = [];
 
-  for (let i = 0; i < oldModule.length; i++) {
+  for (let i = 0; i < moduleLength; i++) {
     const mergedModuleID =
       oldModule[i].moduleID * Constants.moduleIdMult + newModule[i].moduleID;
 
@@ -155,27 +108,39 @@ const calculateChange = (newValue: number, oldValue: number): number => {
   return Number((newValue - oldValue).toFixed(6));
 };
 
-const createAddedModule = (module: ModuleDetails): ComparedModule => {
-  return {
-    ...module,
-    moduleID: module.moduleID,
-    timesCalled: 0,
-    timesCalledChange: module.timesCalled,
-    avgTimePerCall: 0,
-    avgTimePerCallChange: module.avgTimePerCall || 0,
-    totalTime: 0,
-    totalTimeChange: module.totalTime,
-    pcntOfSession: 0,
-    status: "added",
-  };
+const createAddedModule = (modules: ModuleDetails[]): ComparedModule[] => {
+  const comparedModules: ComparedModule[] = [];
+
+  modules.forEach((module) => {
+    comparedModules.push({
+      ...module,
+      moduleID: module.moduleID,
+      timesCalled: 0,
+      timesCalledChange: module.timesCalled,
+      avgTimePerCall: 0,
+      avgTimePerCallChange: module.avgTimePerCall || 0,
+      totalTime: 0,
+      totalTimeChange: module.totalTime,
+      pcntOfSession: 0,
+      status: "added",
+    });
+  });
+
+  return comparedModules;
 };
-const createRemovedModule = (module: ModuleDetails): ComparedModule => {
-  return {
-    ...module,
-    moduleID: module.moduleID * Constants.moduleIdMult,
-    timesCalledChange: -module.timesCalled,
-    avgTimePerCallChange: -(module.avgTimePerCall || 0),
-    totalTimeChange: -module.totalTime,
-    status: "removed",
-  };
+const createRemovedModule = (modules: ModuleDetails[]): ComparedModule[] => {
+  const comparedModules: ComparedModule[] = [];
+
+  modules.forEach((module) => {
+    comparedModules.push({
+      ...module,
+      moduleID: module.moduleID * Constants.moduleIdMult,
+      timesCalledChange: -module.timesCalled,
+      avgTimePerCallChange: -(module.avgTimePerCall || 0),
+      totalTimeChange: -module.totalTime,
+      status: "removed",
+    });
+  });
+
+  return comparedModules;
 };
