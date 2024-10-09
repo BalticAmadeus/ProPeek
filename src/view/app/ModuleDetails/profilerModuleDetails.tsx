0167e09fe3bcca26ab96eebc1c580bd1b4e68073
pjaskutelis <p.jaskutelis@ba.lt>
2024-10-09 11:34:17 +0300

    initial commit

--- src/view/app/ModuleDetails/profilerModuleDetails.tsx
+++ src/view/app/ModuleDetails/profilerModuleDetails.tsx
@@ -121,6 +121,16 @@ function getComparator(sortColumn: string) {
       throw new Error(`unsupported sortColumn: "${sortColumn}"`);
   }
 }
+function jumpToLine(row: LineSummary) {
+  const lineElement = document.getElementById(`line-${row.lineNumber}`);
+  if (lineElement) {
+    document
+      .querySelectorAll(".highlight")
+      .forEach((el) => el.classList.remove("highlight"));
+    lineElement.classList.add("highlight");
+    lineElement.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+}
 
 const ProfilerModuleDetails: React.FC<ProfilerModuleDetailsProps> = ({
   presentationData,
@@ -130,6 +140,75 @@ const ProfilerModuleDetails: React.FC<ProfilerModuleDetailsProps> = ({
   const [moduleRows, setModuleRows] = useState<ModuleDetails[]>(
     presentationData.moduleDetails
   );
+  const fileString = `1   
+2   // function function1 returns integer (pi as integer):
+3   //     define variable iCount as integer no-undo.
+4   //     do iCount = 1 to 25000:
+5   //     end.
+6   //     if pi <> 0 then do:
+7   //         pi = pi - 1.
+8   //         return function1(pi).
+9   //     end.
+10   //     else
+11   //       return 0.
+12   // end function.
+13   
+14   // define variable iCount as integer no-undo initial 1000. aaaaaaaaaaaaaaaaaaaaa
+15   // define variable iNum as integer no-undo.
+16   define variable class1 as class1 no-undo.
+17   define variable class2 as class2 no-undo.
+18   // define variable class3 as class3 no-undo.
+19   // define variable class4 as class4 no-undo.
+20   
+21   class1 = new class1().
+22   class2 = new class2().
+23   // class3 = new class3().
+24   // class4 = new class4().
+25   
+26   class1:method2().
+27   class1:method2(1, 1).
+28   
+29   class2:method2(1).
+30   class2:method2(1, 1).
+31   
+32   // iNum = function1(17).
+33   // class3:method1(7).
+34   
+35   //  do iNum = 1 to 50:
+36   //      class1:method1(iCount).
+37   //      class1:method3().
+38   //  end.
+39   
+40   // iNum = function1(17).
+41   // class3:method1(8).
+42   
+43   // do iNum = 1 to 30:       
+44   //     class2:method1(1000).
+45   //     class2:method3().    
+46   // end.                     
+47   
+48   // iNum = function1(15).
+49   // class3:method1(25).
+50   
+51   // iNum = function1(9).
+52   // class3:method1(9).
+53   
+54   // iNum = function1(20).
+55   // class3:method1(5).
+56   
+57   // class4:method2(20).
+58   // class4:method3(25).
+59   // class4:method4(10).
+60   // class4:method5(15).
+61   
+62   
+63   // iNum = function1(10).
+64   // class3:method1(19).
+65   
+66   // iNum = function1(20).
+67   // class3:method1(10).
+`;
+  const lines = fileString.split("\n");
   const [selectedModuleRow, setSelectedModuleRow] =
     useState<ModuleDetails | null>(null);
   const [sortModuleColumns, setSortModuleColumns] = useState<
@@ -322,7 +401,10 @@ const ProfilerModuleDetails: React.FC<ProfilerModuleDetailsProps> = ({
           <ModuleDetailsTable
             columns={formattedModuleColumns}
             rows={sortedModuleRows}
-            onRowClick={(row) => setSelectedRow(row)}
+            onRowClick={(row) => {
+              setSelectedRow(row);
+              console.log(row);
+            }}
             onRowsChange={setModuleRows}
             sortColumns={sortModuleColumns}
             onSortColumnsChange={setSortModuleColumns}
@@ -386,19 +468,79 @@ const ProfilerModuleDetails: React.FC<ProfilerModuleDetailsProps> = ({
 
       <div className="line-columns">
         <div className="grid-name">Line Summary</div>
-        <DataGrid
-          columns={formattedLineColumns}
-          rows={sortedLineRows}
-          defaultColumnOptions={{
-            sortable: true,
-            resizable: true,
-          }}
-          style={{ textAlign: "end", width: "50%" }}
-          onRowsChange={setSelectedLineRows}
-          sortColumns={sortLineColumns}
-          onSortColumnsChange={setSortLineColumns}
-          onRowDoubleClick={openFileForLineSummary}
-        />
+        <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+          <DataGrid
+            columns={formattedLineColumns}
+            rows={sortedLineRows}
+            defaultColumnOptions={{
+              sortable: true,
+              resizable: true,
+            }}
+            style={{ textAlign: "end", width: "50%" }}
+            onRowsChange={setSelectedLineRows}
+            sortColumns={sortLineColumns}
+            onSortColumnsChange={setSortLineColumns}
+            onRowDoubleClick={openFileForLineSummary}
+            onRowClick={(row) => jumpToLine(row)}
+          />
+          <div
+            className="line-columns"
+            style={{
+              display: "flex",
+              width: "50%",
+              maxHeight: "400px",
+              overflow: "hidden",
+              border: "1px solid var(--vscode-editorWidget-border)",
+              padding: "10px",
+              backgroundColor: "var(--vscode-editor-background)",
+              color: "var(--vscode-editor-foreground)",
+              marginLeft: "5px",
+            }}
+            id="codeContainer"
+          >
+            {/* Wrapper for synchronized scrolling line and code numbers*/}
+            <div
+              style={{
+                display: "flex",
+                overflowX: "auto",
+                overflowY: "auto",
+                maxHeight: "400px",
+                width: "100%",
+              }}
+            >
+              {/* Line numbers column */}
+              <div
+                style={{
+                  textAlign: "right",
+                  paddingRight: "10px",
+                  userSelect: "none",
+                  color: "var(--vscode-editorLineNumber-foreground)",
+                  minWidth: "25px",
+                  backgroundColor: "var(--vscode-editor-background)",
+                }}
+              >
+                {lines.map((_, index) => (
+                  <pre
+                    key={index}
+                    id={`line-${index + 1}`}
+                    style={{ margin: 0 }}
+                  >
+                    {index + 1}
+                  </pre>
+                ))}
+              </div>
+
+              {/* Code lines column */}
+              <div style={{ width: "100%" }}>
+                {lines.map((line, index) => (
+                  <pre key={index} style={{ whiteSpace: "pre", margin: 0 }}>
+                    {line}
+                  </pre>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
